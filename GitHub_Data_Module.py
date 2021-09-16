a4a1e53ccc02a241469128c2e4401c967d54d0c8
pythonroyale <90867253+pythonroyale@users.noreply.github.com>
2021-09-16 13:22:19 -0600

    Add files via upload

---
+++ GitHub_Data_Module.py
@@ -0,0 +1,10 @@
+
+
+"""CW Database for Module_Test_1"""
+
+
+a = 5
+b = 15
+c = 20
+d = 20
+e = 25
---
+++ Main Program File for GitHub.py
@@ -0,0 +1,18 @@
+#Main Program File for GitHub.py
+
+print ()
+
+print ("Welcome to Module Import Program")
+
+import GitHub_Data_Module
+
+num_1 = GitHub_Data_Module.a
+num_2 = GitHub_Data_Module.b
+num_3 = GitHub_Data_Module.c
+num_4 = GitHub_Data_Module.d
+num_5 = GitHub_Data_Module.e
+
+print ("We Will Now Access Data From The Module")
+print (num_1, "+", num_2, "=", num_1 + num_2)
+print (num_3, "*", num_4, "*", num_5, "=", num_3 * num_4 * num_5)
+print ("Magic")
